803e174dbfc67a6887f137d8ecb9b5ebee7862b1
schr0der <jannik.schroeder05@gmail.com>
2023-05-11 14:16:40 +0200

    Moved Searchbar to the left & added filter symbol

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCog, faSliders } from '@fortawesome/free-solid-svg-icons';
 
 const Header: React.FC = () => {
     const [searchValue, setSearchValue] = useState('');
@@ -12,24 +12,25 @@ const Header: React.FC = () => {
     };
 
     return (
-        <header className="fixed top-0 overflow-auto w-full flex p-5 bg-gray-950 text-white z-10 relative sticky">
+        <header className="fixed top-0 overflow-auto w-full flex p-5 bg-gray-950 text-white z-10 relative sticky justify-center">
             <div className="flex items-center">
                 <img src="/path/to/your/logo.png" alt="logo" className="h-10 w-10" />
                 <h1 className="ml-2 text-2xl">Solun Webmail</h1>
             </div>
             <div className="flex-grow mx-10 flex">
-                <div className="flex-grow mx-10 flex items-center justify-center bg-gray-950">
-                    <div className="relative">
+                <div className="flex-grow mx-10 flex items-center justify-start bg-gray-950">
+                    <div className="relative w-1/3">
                         <FontAwesomeIcon icon={faSearch} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
                         <input
-                            className="bg-gray-950 transition-200 text-white block rounded-md pl-10"
+                            className="bg-gray-950 transition-200 text-white block rounded-md pl-10 pr-10 w-full"
                             placeholder="Search"
-                            style={{ width: '100%', height: '40px', border: '1px solid white' }}
+                            style={{ height: '40px', border: '1px solid white' }}
                         />
+                        <FontAwesomeIcon icon={faSliders} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="flex items-center justify-center">
                 <button className="p-2 rounded-md hover:bg-gray-700 transition-200 transition duration-200">
                     <FontAwesomeIcon icon={faCog} />
                 </button>
