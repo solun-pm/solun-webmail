8a1df53623ef1d14a34560a1d34fb4e6bb564623
DanielWTE <daniel.iuser@icloud.com>
2023-06-26 19:31:27 +0200

    updated to mail

--- utils/mailService.ts
+++ utils/mailService.ts
@@ -18,70 +18,63 @@ interface ImapConfig {
     tls: any;
 }
 
-    export const fetchMails = async (fqe: string, password: string) => {
+export const fetchMails = async (fqe: string, password: string) => {
 
     const imapConfig: ImapConfig = {
-        user: 'dw.solun@cyberlinx.de',
-        password: 'beno21',
-        host: 'mail.cyberlinx.de',
-        port: 993,
+        user: fqe,
+        password: password,
+        host: process.env.NEXT_PUBLIC_MAIL_HOST as string,
+        port: process.env.NEXT_PUBLIC_IMAP_PORT as any,
         tls: true,
-        /*user: fqe, // fqe
-        password: password, // user pwd
-        host: process.env.NEXT_PUBLIC_MAIL_HOST,
-        port: process.env.NEXT_PUBLIC_IMAP_PORT,
-        tls: true,*/
     };
 
     const config = {
         imap: imapConfig
     };
 
-    imaps.connect(config).then((connection) => {
-        return connection.openBox('INBOX').then(() => {
-            let mails: Mail[] = [];
-            const searchCriteria = ['ALL'];
-            const fetchOptions = {
-                bodies: ['HEADER', 'TEXT', ''],
-                struct: true
-            };
+    let mails: Mail[] = [];
 
-            return imaps.connect(config).then((connection) => {
-                return connection.openBox('INBOX').then(() => {
-                    const searchCriteria = ['ALL'];
-                    const fetchOptions = {
-                        bodies: ['HEADER', 'TEXT', ''],
-                        struct: true
-                    };
+    try {
+        const connection = await imaps.connect(config);
+        await connection.openBox('INBOX');
+
+        const searchCriteria = ['ALL'];
+        const fetchOptions = {
+            bodies: ['HEADER', 'TEXT', ''],
+            struct: true
+        };
+
+        const messages = await connection.search(searchCriteria, fetchOptions);
+
+        messages.forEach((message) => {
+            const bodyPart = message.parts?.find(part => part.which === 'TEXT');
         
-                    return connection.search(searchCriteria, fetchOptions).then((messages) => {
-                        messages.forEach((message) => {
-                            const bodyPart = message.parts.find(part => part.which === 'TEXT');
-                            console.log("Mail body: ", bodyPart ? bodyPart.body : 'No body found');
-                        });
-                    });
-                });
-            });
+            const mailHeader = message.parts?.find(part => part.which === 'HEADER')?.body;
+        
+            if (mailHeader) {
+                const mail: Mail = {
+                    subject: mailHeader.subject ? mailHeader.subject[0] : '',
+                    date: mailHeader.date ? new Date(mailHeader.date[0]) : new Date(),
+                    body: bodyPart ? bodyPart.body : 'No body found',
+                    senderName: mailHeader.from ? mailHeader.from[0] : '',
+                    senderEmail: mailHeader.to ? mailHeader.to[0] : '',
+                    recipient: mailHeader.cc ? mailHeader.cc[0] : ''
+                };
+        
+                mails.push(mail);
+            }
         });
-    });
+    } catch (error) {
+        console.error('Error fetching mails: ', error);
+    }
+
+    return mails;
 };
 
 // SMTP
 export async function sendMail(username: string, fqe: string, password: string, to: string, cc: string, bcc: string, subject: string, text: string, attachments: any): Promise<void> {
     const transporter = nodemailer.createTransport({
-        host: 'mail.cyberlinx.de',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'dw.solun@cyberlinx.de',
-            pass: 'beno21'
-        },
-        tls: {
-            maxVersion: 'TLSv1.3',
-            minVersion: 'TLSv1.2',
-            ciphers: 'TLS_AES_128_GCM_SHA256',
-        }
-        /*host: process.env.NEXT_PUBLIC_MAIL_HOST,
+        host: process.env.NEXT_PUBLIC_MAIL_HOST,
         port: Number(process.env.NEXT_PUBLIC_SMTP_PORT),
         secure: true, // true for 465, false for other ports
         auth: {
@@ -92,7 +85,7 @@ export async function sendMail(username: string, fqe: string, password: string, 
             maxVersion: 'TLSv1.3',
             minVersion: 'TLSv1.2',
             ciphers: 'TLS_AES_128_GCM_SHA256',
-        }*/
+        }
     });
     let info = await transporter.sendMail({
         from: `"${username}" <${fqe}>`, // sender address
