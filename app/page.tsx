64be83c87c006bdd00d3605bef92214fe134e981
DanielWTE <daniel.iuser@icloud.com>
2023-07-12 16:28:56 +0200

    Added dev mode

--- app/page.tsx
+++ app/page.tsx
@@ -6,70 +6,91 @@ import DisableScroll from '../components/disablescroll';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-    const router = useRouter();
+  const router = useRouter();
+  const [userInfo, setUserInfo] = useState(null) as any;
+  const [userDetails, setUserDetails] = useState(null) as any;
+  const devMode = true;
 
-    const [userInfo, setUserInfo] = useState(null) as any;
-    const [userDetails, setUserDetails] = useState(null) as any;
+  useEffect(() => {
+      const fetchUserInfo = async () => {
+          const token = localStorage.getItem("jwt");
 
-    useEffect(() => {
-        const fetchUserInfo = async () => {
-        const token = localStorage.getItem("jwt");
-
-        if (!token) {
-            router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
-            return;
-        }
-
-        const response = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/webmail/jwt_details", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              token: token,
-              onlyVerify: false,
-            }),
-          });
-
-            if (!response.ok) {
-                localStorage.removeItem("jwt");
-                router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
-                return;
-            }
-            const data = await response.json();
-            setUserInfo(data);
-
-        const detailsResponse = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/webmail/user_details", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              user_id: data.user_id,
-            }),
-          });
-          if (!detailsResponse.ok) {
-            localStorage.removeItem("jwt");
-            router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
-            return;
+          if (!token) {
+              router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
+              return;
           }
 
-            const detailsData = await detailsResponse.json();
-            setUserDetails(detailsData);
-        };
-        fetchUserInfo();
-    }, [router]);
+          const response = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/webmail/jwt_details", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                  token: token,
+                  onlyVerify: false,
+              }),
+          });
 
-    if (!userInfo || !userDetails) {
-        return null;
-    }
+          if (!response.ok) {
+              localStorage.removeItem("jwt");
+              router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
+              return;
+          }
+          const data = await response.json();
+          setUserInfo(data);
 
-    return (
-        <div className="">
-            <section>
-                <DisableScroll />
-                <Dashboard userInfo={userInfo} userDetails={userDetails} />
-            </section>
-        </div>
-    )
+          const detailsResponse = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/webmail/user_details", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                  user_id: data.user_id,
+              }),
+          });
+          if (!detailsResponse.ok) {
+              localStorage.removeItem("jwt");
+              router.push(process.env.NEXT_PUBLIC_AUTH_DOMAIN + '/login/mail');
+              return;
+          }
+
+          const detailsData = await detailsResponse.json();
+          setUserDetails(detailsData);
+      };
+
+      if (!devMode) {
+          fetchUserInfo();
+      } else {
+          setUserInfo({
+              user_id: 1,
+              username: "test",
+              domain: "@solun.pm",
+              fqe: "test@solun.pm",
+              password: null,
+              membership: "free",
+              private_key: null,
+              public_key: null
+          });
+          setUserDetails({
+              fqe: "test@solun.pm",
+              username: "test",
+              user_id: 1,
+              private_key: null,
+              password: null,
+          });
+      }
+  }, [router, devMode]);
+
+  if (!userInfo || !userDetails) {
+      return null;
+  }
+
+  return (
+      <div className="">
+          <section>
+              <DisableScroll />
+              <Dashboard userInfo={userInfo} userDetails={userDetails} />
+          </section>
+      </div>
+  );
 }
